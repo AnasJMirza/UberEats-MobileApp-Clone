47e289f127738bfc97be3983d255c179bafaa7a1
AnasJMirza <ajmhimself@gmail.com>
2022-07-23 02:08:05 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={styles.container} >
+      <Text style={styles.text} >Yoo! Let's build Uber Eats Clone 🔥</Text>
+    </View>
+  )
+}
+
+export default App
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#030012", 
+  },
+
+  text: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: "600",
+  }
+})
