7cf6abd77db87f0236e141363cb56a4f199552f3
AnasJMirza <ajmhimself@gmail.com>
2022-08-04 02:14:00 +0500

    add buttons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,24 @@
 import { View, Text } from 'react-native'
+import { StyleSheet } from "react-native";
 import React from 'react'
 import HeaderBtn from '../headerBtn/HeaderBtn';
 
 const Header = () => {
   return (
-    <View>
-      <HeaderBtn title = "Abc" />
-      <HeaderBtn title = "Xyz" />
+    <View style={styles.container}>
+      <HeaderBtn title = "Delivery" backgroundColor="black"  color="white" />
+      <HeaderBtn title = "Pickup" backgroundColor="white"  color="black" />
     </View>
   )
 };
 
 export default Header;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+      flexDirection: "row",
+      alignSelf: "center",
+  }
+});
--- src/components/headerBtn/HeaderBtn.js
+++ src/components/headerBtn/HeaderBtn.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet } from "react-native";
 import React from 'react'
 
 const HeaderBtn = (props) => {
   return (
-    <View>
-      <Text> { props.title } </Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.btnText}> { props.title } </Text>
+    </TouchableOpacity>
   )
 };
-
 export default HeaderBtn;
+
+
+
+// Styling the component
+
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: "black",
+      paddingHorizontal: 16,
+      paddingVertical: 6,
+      borderRadius: 30,
+  },
+
+  btnText: {
+      color: "white",
+      fontSize: 15,
+      fontWeight: "900",
+  }
+});
