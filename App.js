e0f3e7f75d47e881776904f8f6e495124e01cac6
AnasJMirza <ajmhimself@gmail.com>
2022-08-04 03:36:15 +0500

    style the statusbar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import Home from './src/screens/Home';
 
 const App = () => {
   return (
     <View style={{backgroundColor: "#eee", flex: 1}}>
+      <StatusBar backgroundColor="#eee" barStyle="dark-content" />
       <Home />
     </View>
   )
